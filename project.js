79bd23e6288a39942cc0a897f995545e71bfc9d6
Mihajlo Zoric <110368940+zorickomerc@users.noreply.github.com>
2023-07-26 17:23:46 +0200

    2nd commit

--- project.js
+++ project.js
@@ -8,7 +8,22 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS =  3;
 
+const SYMBOL_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8,
+}
+
+const SYMBOL_VALUES = {
+    A:5,
+    B: 4,
+    C: 3, 
+    D: 2
+}
 
 
 const deposit = () => {
@@ -49,6 +64,18 @@ const getBet = (balance, lines) => {
             }
         }
 }
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOL_COUNT)) {
+        for(let i = 0; i < count; i++){
+            symbols.push(symbol);
+        }
+        console.log(symbol)
+    }
+}
+
+
 
 let balance = deposit();
 const numberOflines = getNumberOfLines();
